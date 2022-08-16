dfe0b0837536ce3ca3f5b476e46b20e7e0b33b9a
Marion Devroede <marion.devroede@hotmail.fr>
2022-08-16 23:25:00 +0200

    Modif erreur

---
+++ back/middleware/multer-config.js
@@ -0,0 +1,21 @@
+const multer = require('multer');
+
+const MIME_TYPES = {
+  'image/jpg': 'jpg',
+  'image/jpeg': 'jpg',
+  'image/png': 'png'
+};
+//Configurer le chemin et le nom de fichier pour les fichiers entrants
+const storage = multer.diskStorage({
+  destination: (req, file, callback) => {
+    callback(null, 'images');
+  },
+  filename: (req, file, callback) => {
+    const name = file.originalname.split(' ').join('_');
+    const extension = MIME_TYPES[file.mimetype];
+    callback(null, name + Date.now() + '.' + extension);
+  }
+});
+
+//capture les fichiers de type image et les enregistres grâce à storage
+module.exports = multer({storage: storage}).single('image');
